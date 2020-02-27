54ff45716fbfd78b95c2dd1ea5a08830899d63e1
DAEUN28 <acone1128@gmail.com>
2020-02-27 23:57:08 +0900

    Update package
    Formatting
    Add server third party in target dependencies

--- Package.swift
+++ Package.swift
@@ -4,15 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "Mongli-Server",
-    dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura", from: "2.8.0"),
-        .package(url: "https://github.com/IBM-Swift/SwiftKueryMySQL.git", from: "2.0.2"),
-        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.9.0")
-    ],
-    targets: [
-        .target(name: "Mongli-Server", dependencies: ["Kitura"]),
-        .testTarget(name: "Mongli-ServerTests",
-                    dependencies: ["Mongli-Server", "SwiftKueryMySQL", "HeliumLogger"])
-    ]
+  name: "Mongli-Server",
+  dependencies: [
+    .package(url: "https://github.com/IBM-Swift/Kitura", from: "2.8.0"),
+    .package(url: "https://github.com/IBM-Swift/SwiftKueryMySQL.git", from: "2.0.2"),
+    .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.9.0")
+  ],
+  targets: [
+    .target(name: "Mongli-Server", dependencies: ["Kitura", "SwiftKueryMySQL", "HeliumLogger"]),
+    .testTarget(name: "Mongli-ServerTests" , dependencies: [.target(name: "Mongli-Server"), "Kitura" ])
+  ]
 )
