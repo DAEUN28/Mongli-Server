f51464894abf656be1c0d9f5cc71d759b449141a
DAEUN28 <acone1128@gmail.com>
2020-03-05 23:06:49 +0900

    Add SecurityInformation
    Add SecurityInformation for server's security

--- Sources/Mongli-Server/Application/Application.swift
+++ Sources/Mongli-Server/Application/Application.swift
@@ -2,20 +2,29 @@ import Foundation
 
 import Kitura
 import KituraContracts
+import SwiftJWT
 import SwiftKueryMySQL
 import SwiftKuery
 
 public class App {
 
+  // MARK: Database
   private static let poolOptions = ConnectionPoolOptions(initialCapacity: 1, maxCapacity: 5)
-  private static let connectionURL = URL(string: "mysql://root:1128@127.0.0.1:5252/Mongli")!
-
+  private static let connectionURL = URL(string: SecurityInformation.databaseURL)!
   private let pool = MySQLConnection.createPool(url: connectionURL, poolOptions: poolOptions)
-  
+
+  // MARK: JWT
+  private static let jwtSigner = JWTSigner.rs256(privateKey: SecurityInformation.privateKey)
+  private static let jwtVerifier = JWTVerifier.rs256(publicKey: SecurityInformation.publicKey)
+  private let jwtEncoder = JWTEncoder(jwtSigner: jwtSigner)
+  private let jwtDecoder = JWTDecoder(jwtVerifier: jwtVerifier)
+
+  // MARK: Kitura
   private let router = Router()
 
   public init() {
-
+    router.encoders[MediaType(type: .application, subType: "jwt")] = { return self.jwtEncoder }
+    router.decoders[MediaType(type: .application, subType: "jwt")] = { return self.jwtDecoder }
   }
 
   public func run() {
