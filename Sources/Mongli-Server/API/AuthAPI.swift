4591c7d1360683ee0c175c4439b38959da60c721
DAEUN28 <acone1128@gmail.com>
2020-03-08 00:18:05 +0900

    Add signIn API

---
+++ Sources/Mongli-Server/API/AuthAPI.swift
@@ -0,0 +1,70 @@
+import Foundation
+
+import Kitura
+import KituraContracts
+import LoggerAPI
+import SwiftJWT
+import SwiftKueryMySQL
+import SwiftKuery
+import AuthenticationServices
+
+
+extension App {
+  func signInHandler(auth: Auth, completion: @escaping (Token?, RequestError?) -> Void) {
+    if let name = auth.name {
+      self.pool.getConnection { [weak self] connection, error in
+        guard let self = self, let connection = connection else {
+          Log.error(error?.localizedDescription ?? "connectionError")
+          return completion(nil, .internalServerError)
+        }
+
+        connection.execute(query: QueryManager.createUser(auth.uid, name: name).query()) { result in
+          if let error = result.asError {
+            Log.error(error.localizedDescription)
+            return completion(nil, .internalServerError)
+          }
+          
+          connection.execute(query: QueryManager.readUserID(auth.uid).query()) { result in
+            guard let resultSet = result.asResultSet else {
+              Log.error(result.asError?.localizedDescription ?? "readUserIDQueryError")
+              return completion(nil, .internalServerError)
+            }
+
+            resultSet.forEach(operation: { row, error in
+              guard let row = row else { return }
+
+              if let error = error {
+                Log.error(error.localizedDescription)
+                return completion(nil, .internalServerError)
+              }
+
+              guard let id = row.first as? Int32,
+                let accessToken = self.tokenManager.createToken(AccessTokenClaim(sub: Int(id))),
+                let refreshToken = self.tokenManager.createToken(RefreshTokenClaim(sub: Int(id))) else {
+                Log.error("createTokenError")
+                return completion(nil, .internalServerError)
+              }
+
+              self.pool.getConnection { connection, error in
+                guard let connection = connection else {
+                  Log.error(error?.localizedDescription ?? "connectionError")
+                  return completion(nil, .internalServerError)
+                }
+
+                connection.execute(query: QueryManager.updateRefreshToken(refreshToken, id: Int(id)).query()) { result in
+                  if let error = result.asError {
+                    Log.error(error.localizedDescription)
+                    return completion(nil, .internalServerError)
+                  }
+
+                  let response = Token(accessToken: accessToken, refreshToken: refreshToken)
+                  return completion(response, .created)
+                }
+              }
+            })
+          }
+        }
+      }
+    }
+  }
+}
