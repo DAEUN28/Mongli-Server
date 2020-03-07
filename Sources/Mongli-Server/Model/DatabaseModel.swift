402d6c034e7346f198ac123d45e59cc44f870b8d
DAEUN28 <acone1128@gmail.com>
2020-03-08 00:17:10 +0900

    Update DatabaseModel

--- Sources/Mongli-Server/Model/DatabaseModel.swift
+++ Sources/Mongli-Server/Model/DatabaseModel.swift
@@ -5,20 +5,19 @@ import SwiftKuery
 
 class UserTable: Table {
   let tableName = "User"
-  let id = Column("id", String.self, primaryKey: true)
-  let number = Column("number", Int32.self)
-  let name = Column("name", String.self)
-  let accessToken = Column("accessToken", String.self)
-  let refreshToken = Column("refreshToken", String.self)
+  let id = Column("id", primaryKey: true)
+  let uid = Column("uid")
+  let name = Column("name")
+  let refreshToken = Column("refreshToken", notNull: false)
 }
 
 class DreamTable: Table {
   let tableName = "Dream"
-  let id = Column("id", Int32.self, primaryKey: true)
-  let uid = Column("uid", Int32.self)
-  let date = Column("date", SQLDate.self)
-  let category = Column("category", Int32.self)
-  let title = Column("title", String.self)
-  let content = Column("content", String.self)
+  let id = Column("id", primaryKey: true)
+  let userID = Column("UserID")
+  let date = Column("date")
+  let category = Column("category")
+  let title = Column("title")
+  let content = Column("content")
 }
 
