c851450a902f05dcd5d9e21d93848687ee71de22
DAEUN28 <acone1128@gmail.com>
2020-04-01 22:42:14 +0900

    Update Kitura version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "Mongli-Server",
   dependencies: [
-    .package(url: "https://github.com/IBM-Swift/Kitura", from: "2.8.0"),
+    .package(url: "https://github.com/IBM-Swift/Kitura", from: "2.9.1"),
     .package(url: "https://github.com/IBM-Swift/SwiftKueryMySQL.git", from: "2.0.2"),
     .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.9.0"),
     .package(url: "https://github.com/IBM-Swift/Swift-JWT.git", from: "3.6.1")
